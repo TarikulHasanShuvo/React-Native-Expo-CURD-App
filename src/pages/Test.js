0fd2afd18ffb35ca14284f2c04f6c52376e480df
Tarikul Hasan Shuvo <tarikul15-7944@diu.edu.bd>
2022-02-13 18:16:44 +0600

    add local storage

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button, Card, Layout, Text,Spinner } from '@ui-kitten/components';
+import React, {useEffect} from 'react';
+import {Alert, StyleSheet, View} from 'react-native';
+import {Button, Card, Layout, Spinner, Text} from '@ui-kitten/components';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoadingIndicator = (props) => (
     <View style={[props.style, styles.indicator]}>
-        <Spinner size='small'  status='success'/>
+        <Spinner size='small' status='success'/>
     </View>
 );
 const Header = (props) => (
@@ -14,6 +15,13 @@ const Header = (props) => (
     </View>
 );
 
+const showMessage = () => {
+    AsyncStorage.getItem('message').then(res => {
+        Alert.alert(`${res}`);
+    })
+
+}
+
 const Footer = (props) => (
     <View {...props} style={[props.style, styles.footerContainer]}>
         <Button
@@ -22,38 +30,47 @@ const Footer = (props) => (
             status='basic'>
             CANCEL
         </Button>
-        <Button
-            style={styles.footerControl}
-            size='small'>
-            ACCEPT
+        <Button onPress={showMessage}
+                style={styles.footerControl}
+                size='small'>
+            Show Message
         </Button>
     </View>
 );
 
- const Test = () => (
-    <React.Fragment>
-        <Layout style={styles.topContainer} level='1'>
+const Test = () => {
+    useEffect(() => {
+        AsyncStorage.setItem('message', 'This a new warning message!')
+    }, [])
 
-            <Card style={styles.card} header={Header}  status='primary'>
-                <Text>With Header</Text>
+
+    return (
+        <>
+            <Layout style={styles.topContainer} level='1'>
+
+                <Card style={styles.card} header={Header} status='primary'>
+                    <Text>With Header</Text>
+                </Card>
+
+                <Card style={styles.card} footer={Footer} status='warning'>
+                    <Text>With Footer</Text>
+                </Card>
+
+            </Layout>
+
+            <Card style={styles.card} header={Header} footer={Footer}>
+                <Text>
+                    The Maldives, officially the Republic of Maldives, is a small country in South Asia, located in the
+                    Arabian Sea
+                    of the Indian Ocean. It lies southwest of Sri Lanka and India, about 1,000 kilometres (620 mi) from
+                    the Asian
+                    continent
+                </Text>
             </Card>
 
-            <Card style={styles.card} footer={Footer}  status='warning'>
-                <Text>With Footer</Text>
-            </Card>
-
-        </Layout>
-
-        <Card style={styles.card} header={Header} footer={Footer}>
-            <Text>
-                The Maldives, officially the Republic of Maldives, is a small country in South Asia, located in the Arabian Sea
-                of the Indian Ocean. It lies southwest of Sri Lanka and India, about 1,000 kilometres (620 mi) from the Asian
-                continent
-            </Text>
-        </Card>
-
-    </React.Fragment>
-);
+        </>
+    )
+};
 
 export default Test;
 
